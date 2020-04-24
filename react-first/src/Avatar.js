710729cf5053c674e33f5a6f3ddbb2ee14dc63ab
ashu9999 <kmr.ashutosh9999@gmail.com>
2020-04-24 22:53:59 +0530

    init avatar

---
+++ react-first/src/Avatar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Avatar.css';
+
+const Avatar = () => {
+    return(
+        <div className="avatarstyle">
+            <img src= "https://joeschmoe.io/api/v1/ashu" alt="ashu"></img>
+            <h1>ashu</h1>
+            <p>Web dev</p>
+        </div>
+    )
+}
+export default Avatar;
